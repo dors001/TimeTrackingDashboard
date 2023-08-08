ded1bb3f879aed50e0aa9d1a326e7d281589834f
dors001 <dorarishani@gmail.com>
2023-08-08 03:10:25 +0300

    Define grid layout

--- Time Tracking Dashboard/src/App.tsx
+++ Time Tracking Dashboard/src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        gap={4}
+        margin="25vh 16.5vw"
+      >
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 1
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 2
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 3
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 4
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 5
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 6
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 7
+          </Box>
+        </GridItem>
+        <GridItem>
+          <Box w="100%" h="245px">
+            Block 8
+          </Box>
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
